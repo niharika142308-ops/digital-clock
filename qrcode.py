d3d1f21737489629809882f5211c028641abd309
niharika142308-ops <67116930+niharika142308-ops@users.noreply.github.com>
2021-04-05 15:58:47 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,13 +1,1 @@
-'''
-Create your own QR Code with Python
-Author: Niharika
-'''
-#import the module
-import pyqrcode
 
-#define the data
-data = "https://github.com/niharika142308-ops"
-#create qrcode
-img = pyqrcode.create(data)
-#save the qrcode in png format with proper scaling
-img.png('githubwebsite.png',scale=10)
